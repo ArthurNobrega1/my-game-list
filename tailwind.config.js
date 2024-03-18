979581100565d4e1d65b258c826203f13b9d5b0a
fukczun <arthurnobrega906@gmail.com>
2024-03-18 19:37:08 -0300

    feat(tailwind): Adicionar configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /* @type {import('tailwindcss').Config} */
-
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif']
+      },
+      colors: {
+        'light-gray': '#D9D9D9',
+        'light-green': '#B4D9CE',
+        'dark-green': '#184D59',
+      }
+    }
+  },
+  plugins: [],
+}
+
