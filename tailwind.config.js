500e334dea4a8b66e31654c74e581641b5740839
fukczun <arthurnobrega906@gmail.com>
2024-03-19 10:17:59 -0300

    chore(tailwind): Adicionar valores na config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,22 @@ module.exports = {
         'light-gray': '#D9D9D9',
         'light-green': '#B4D9CE',
         'dark-green': '#184D59',
+      },
+      margin: {
+        'calc(42px,header)': 'calc(calc(.003*calc(100vw - 1440px)) + calc(8.91667vw - 8.4375vh))',
+        'calc(42px,inputGroup)': 'calc(calc(.0003*calc(100vw - 1440px)) + calc(2.91176vw + .0069vh))',
+        'calc(-18px)': 'calc(calc(.003*calc(100vw - 1440px)) + calc(3.75vw - 7.03125vh))'
+      },
+      gap: {
+        '18': '6.977%'
+      },
+      padding: {
+        '19': '3.64%'
       }
+    },
+    borderRadius: {
+      md: '0.625rem',
+      lg: '0.9375rem'
     }
   },
   plugins: [],
