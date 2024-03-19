99444ff62b1480a2f25f002bdc90d6464c19f8db
fukczun <arthurnobrega906@gmail.com>
2024-03-19 16:28:21 -0300

    feat(login): Adicionar tela login; Corrigir valores relativos antigos; Alterar title da página principal; Definir limites para tamanhos relativos de componentes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import {
 } from "react-router-dom";
 
 import SignUp from './pages/SignUp'
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
--- src/components/ButtonSubmit.jsx
+++ src/components/ButtonSubmit.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ButtonSubmit = props => 
-    <button className="font-semibold w-[38.89%] h-[4.16667vw] block bg-light-green mx-auto text-dark-green text-[max(1.805556vw,.78125rem)] rounded-md" type="submit">
+    <button className="font-semibold w-[max(14.09723vw,4.63125rem)] h-[max(4.16667vw,1.36875rem)] block bg-light-green mx-auto text-dark-green text-[max(1.805556vw,.78125rem)] rounded-md" type="submit">
         {props.label}
     </button>
 
