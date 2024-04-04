85f73989b38cdc1de41469436e05accb8bfa9b6f
fukczun <arthurnobrega906@gmail.com>
2024-04-04 09:17:04 -0300

    style(card && nav): Deixar responsivo no home && feat(nav): Deixar possivel troca de nav por isLogged

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Nav = props =>
     <nav className="font-extralight mr-[.9375rem] text-[.90278vw] max-sm:m-0">
-        <ul className="h-full flex gap-9px *:my-auto">
-            <li className="text-light-green text-[max(.90278vw,.43125rem)] hover:text-light-green-600"><u>SUPORTE</u></li>
+        <ul className="h-full flex gap-3 mr-1 *:my-auto">
+            <li className="text-light-green text-[max(.90278vw,.73125rem)] uppercase hover:text-light-green-600"><u>suporte</u></li>
             {props.elements.map((element, index) => <li key={index}>{element}</li> )}
         </ul>
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,15 +7,19 @@ import Card from '../components/Card'
 function Home() {
     const defaultArray = Array(5).fill("Nome do Jogo")
 
+    const isLoged = " "
+    const navNotLogged = [<Button label="CADASTRAR" redirect="signup"/>, <Button label="ENTRAR" redirect="login"/>]
+    const navLogged = [<u className="text-light-green text-[max(.90278vw,.73125rem)] uppercase hover:text-light-green-600">sair</u>, <img className="my-auto h-[max(4.157vh,1.50625rem)]" src="./user.png" alt="Icon de Usuário" />]
+
     return (
         <div>
             <header className="font-inter w-[max(100%,9.7rem)] h-[max(6.434vh,2.33675rem)] flex justify-between bg-dark-green ">
                 <img className="h-[max(8.557vh,3.3109375rem)] my-auto " src="./logo-contrast.png" alt="Logo do My Game List Contraste" />
-                <Nav elements={[<Button label="CADASTRAR" redirect="signup"/>, <Button label="ENTRAR" redirect="login"/>]}/>
+                <Nav elements={isLoged ? navLogged : navNotLogged}/>
             </header>
             <main className="bg-home w-[max(100%,9.7rem)] h-[max(93.528vh,33rem)]">
                 <section className="h-[47.506vh]"></section>
-                <aside className="flex max-sm:flex-col justify-around max-sm:items-center bg-dark-green w-full h-[29.0625rem] max-sm:h-[170vh] rounded-t-lg">
+                <aside className="flex max-sm:flex-col justify-around max-sm:items-center bg-dark-green w-full h-[29.0625rem] max-lg:h-[46.1vh] max-md:h-[47vh] max-sm:h-[150vh] rounded-t-lg">
                     <Card subtitle="Top Avaliados" games={defaultArray}/>
                     <Card subtitle="Top Jogados" games={defaultArray}/>
                     <Card subtitle="Top Jogando" games={defaultArray}/>
