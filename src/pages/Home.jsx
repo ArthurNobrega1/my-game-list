02d859b5ce5034ec1533d4b2e2762757c88ba153
fukczun <arthurnobrega906@gmail.com>
2024-04-04 08:25:18 -0300

    style(header < home): Descomponetizar o header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Header from '../components/Header'
+import Nav from "../components/Nav";
 import Button from '../components/Button';
 import Card from '../components/Card'
 
@@ -9,7 +9,10 @@ function Home() {
 
     return (
         <div>
-            <Header elements={[<Button label="CADASTRAR" redirect="signup"/>, <Button label="ENTRAR" redirect="login"/>]} />
+            <header className="font-inter w-[max(100%,9.7rem)] h-[max(6.434vh,2.33675rem)] flex justify-between bg-dark-green ">
+                <img className="h-[max(8.557vh,3.3109375rem)] my-auto " src="./logo-contrast.png" alt="Logo do My Game List Contraste" />
+                <Nav elements={[<Button label="CADASTRAR" redirect="signup"/>, <Button label="ENTRAR" redirect="login"/>]}/>
+            </header>
             <main className="bg-home w-[max(100%,9.7rem)] h-[max(93.528vh,33rem)]">
                 <section className="h-[47.506vh]"></section>
                 <aside className="flex max-sm:flex-col justify-around max-sm:items-center bg-dark-green w-full h-[29.0625rem] max-sm:h-[170vh] rounded-t-lg">
