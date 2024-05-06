7335cd1029afa0acd126d2ff3b1d930d942cec55
fukczun <arthurnobrega906@gmail.com>
2024-05-06 13:37:10 -0300

    style(profile): Deixar responsivo

--- src/components/SectionGame.jsx
+++ src/components/SectionGame.jsx
@@ -3,16 +3,14 @@ import React from "react";
 const SectionGame = props => {
     let games
     if (!props.games)  games = Array(5).fill(<div className="bg-light-gray w-[11.375rem] h-[6.125rem]"></div>)
-    else games = [0, 1, 2, 3, 4].map(index => games[index] ? games[index] : <div className="bg-light-gray w-[11.375rem] h-[6.125rem]"></div> )
+    else games = [0, 1, 2, 3, 4].map(index => games[index] ? games[index] : <div className="bg-light-gray h-[6.125rem] w-[11.375rem]"></div> )
 
     return (
-    <section className="space-y-[-.9rem] mb-3">
+    <section className="space-y-[-.9rem] mb-3 max-sm:w-screen">
         <p className="text-white mb-2">
             {props.icon}<span className="uppercase">{props.title}</span>
         </p>
-        <div className="flex gap-8 ml-7 border-t-2 pt-5 w-[90%]">
-            {games.map(game => game)}
-        </div>
+        <div className="overflow-x-auto flex gap-8 *:shrink-0 ml-7 border-t-2 pt-5 w-[90%]">{games}</div>
     </section>)
     }
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -35,16 +35,16 @@ function Profile() {
             </header>
             <main className="bg-light-green h-[93.566vh]">
                 <div className="relative h-[31%] w-[70%]">
-                    <div className="flex flex-col h-[max(24.38vh,8.216rem)] justify-center">
-                        <div className="flex justify-between ml-10 items-center font-josefin font-normal text-light-green-700">
+                    <div className="flex flex-col h-[max(24.38vh,8.216rem)] justify-center max-sm:w-screen">
+                        <div className="flex max-sm:flex-col justify-between ml-10 max-sm:ml-0 items-center font-josefin font-normal text-light-green-700 max-sm:gap-3">
                             <div className="flex items-center gap-5">
-                                <img className="size-[7.9028vw]" src="./user-big.png" alt="Usuário Grande" />
+                                <img className="size-[max(7.9028vw,4.089rem)]" src="./user-big.png" alt="Usuário Grande" />
                                 <header>
-                                    <h1 className="text-[1.6rem]">Nome</h1>
-                                    <h2 className="text-[1.25rem]">Bio</h2>
+                                    <h1 className="text-[max(2.22222vw,.9rem)]">Nome</h1>
+                                    <h2 className="text-[max(1.38889vw,.6189rem)]">Bio</h2>
                                 </header>
                             </div>
-                            <div className="flex gap-5 text-[1.25rem]">
+                            <div className="flex gap-5 text-[max(1.38889vw,.6189rem)]">
                                 {Object.keys(infos).map(key =>
                                     <div className="flex flex-col items-center">
                                         <p>{infos[key]}</p>
@@ -53,12 +53,12 @@ function Profile() {
                             </div>
                         </div>
                     </div>
-                    <div className="flex absolute bottom-0 gap-2 w-full">
+                    <div className="flex absolute bottom-0 gap-2 w-full max-sm:w-screen">
                         {subSessoes.map((subSessao, index) => <Button2 key={`button-${index}`} label={subSessao} />)}
                     </div>
                 </div>
                 <div className="min-h-[69%] h-min bg-light-green-700">
-                    <div className="flex flex-col ml-5 pt-8">
+                    <div className="flex flex-col ml-5 pt-8 max-sm:ml-0">
                         {sectionGames.titles.map((titulo, index) => <SectionGame title={titulo} icon={<img className="inline" src={sectionGames.icons[index]} alt={sectionGames.descricao[index]} games={sectionGames.games[index]} />} />)}
                     </div>
                 </div>
