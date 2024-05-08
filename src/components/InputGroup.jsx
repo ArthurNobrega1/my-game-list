0f6207ec34c34d0cbc7ed1ff3a50664cd00983dc
fukczun <arthurnobrega906@gmail.com>
2024-05-08 08:46:43 -0300

    feat(Input Login SignUp): Adicionar required aos inputsGroup

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -3,6 +3,6 @@ import React from "react";
 import Input from './Input'
 
 const InputGroup = (props) => 
-    props.inputs.map((input, index) => <Input {...input} key={`input-${index}`}/>)       
+    props.inputs.map((input, index) => <Input required {...input} key={`input-${index}`}/>)       
 
 export default InputGroup
