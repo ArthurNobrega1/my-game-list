4ab6949f58fad789e3e40d9be00f142c4635a95a
fukczun <arthurnobrega906@gmail.com>
2024-06-07 10:26:43 -0300

    fix(cursor buttonVerMais): Resolver bug do ponteiro

--- src/components/ButtonVerMais.jsx
+++ src/components/ButtonVerMais.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { verMais } from "../assets/tools";
 
 const ButtonVerMais = props => 
-    <button onClick={() => verMais(props.setGamesVisiveis, props.games, props.id)} className="verMais cursor-pointer focus:cursor-auto hover:text-light-green-600">
+    <button onClick={() => verMais(props.setGamesVisiveis, props.games, props.id)} className="verMais hover:text-light-green-600">
         Ver mais...
     </button>
 
